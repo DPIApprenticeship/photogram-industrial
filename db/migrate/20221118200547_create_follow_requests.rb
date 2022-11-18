61376ac7ce29136b54c8d78ecd11ce659ca57039
mpuchyr <matt.puchyr@gmail.com>
2022-11-18 20:12:48 +0000

    Create and run migration for follow requests

---
+++ db/migrate/20221118200547_create_follow_requests.rb
@@ -0,0 +1,13 @@
+class CreateFollowRequests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :follow_requests do |t|
+      # t.references :recipient, null: false, foreign_key: true
+      # t.references :sender, null: false, foreign_key: true
+      t.references :recipient, null: false, foreign_key: {to_table: :users}, index: true
+      t.references :sender, null: false, foreign_key: {to_table: :users}, index: true
+      t.string :status, default: "pending"
+
+      t.timestamps
+    end
+  end
+end
