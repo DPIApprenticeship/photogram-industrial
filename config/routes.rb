5e5c3a9984ef620e27bb7ced12a5058e2fbfb41b
mpuchyr <matt.puchyr@gmail.com>
2022-11-28 22:29:39 +0000

    Create show method in users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :photos
   devise_for :users
   root "photos#index"
+
+  # get "/users/:id" => "users#show", as: :user
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
